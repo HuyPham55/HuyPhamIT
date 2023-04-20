568729ca6edc6e0df8501b07982fa93099e9eeb7
huydu <huyducphamofficial@gmail.com>
2023-04-20 21:59:10 +0700

    Feat: (FE) Labeling update

---
+++ lang/zh/menu.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+  'categories' => '分類',
+  'static_pages' => '自由頁管理',
+  'contact_us' => '聯絡我們',
+  'contact_requests' => '聯繫請求',
+  'website_settings' => '一般設定',
+  'settings' => '系統設定',
+  'account_settings' => '帳號管理',
+  'profile' => '賬戶設置',
+  'change_password' => '修改密碼',
+  'accounts_and_permissions' => '管理者與權限設定',
+  'roles' => '權限',
+  'users' => '管理者',
+);
