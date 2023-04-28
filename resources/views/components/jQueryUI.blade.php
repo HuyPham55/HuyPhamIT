297a4abbedc4b07fcbbf8cd7c2a2a7d6fbbe36a3
huydu <huyducphamofficial@gmail.com>
2023-04-28 18:43:38 +0700

    Feat: (BE) Added example for dropzone vs sortable element
    Added jQueryUI library

---
+++ resources/views/components/jQueryUI.blade.php
@@ -0,0 +1,6 @@
+@push('css')
+    <link rel="stylesheet" href="/backend/assets/libs/jquery-ui-1.13.2/jquery-ui.min.css"/>
+@endpush
+@push('js')
+    <script src="/backend/assets/libs/jquery-ui-1.13.2/jquery-ui.js"></script>
+@endpush
