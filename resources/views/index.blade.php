7ee100970789a1f72a3b4371c9829bc47be61ae1
huy <huyducphamofficial@gmail.com>
2024-10-10 00:56:29 +0700

    Feat: Add needed Frontend packages. Added Example component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    @vite(['resources/js/app.js'])
+    @vite(['resources/ts/app.ts'])
     <title>{{config('app.name')}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset(cachedOption('site_favicon')) }}"/>
