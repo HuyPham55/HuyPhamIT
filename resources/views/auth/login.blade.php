45d1f58f9ab7f2da9bf91f317bc4b7fa1fbe6722
huy <huyducphamofficial@gmail.com>
2024-10-26 19:20:31 +0700

    Feat: Implement login page with icheck bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -81,7 +81,7 @@
         {{-- Login field --}}
         <div class="row">
             <div class="col-7">
-                <div class="form-check icheck-primary" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
+                <div class="icheck-primary" title="{{ __('adminlte::adminlte.remember_me_hint') }}">
                     <input class="form-check-input" type="checkbox" name="remember"
                            id="remember" {{ old('remember') ? 'checked' : '' }}>
                     <label for="remember" class="form-check-label">
