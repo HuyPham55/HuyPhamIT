06704e0a5f083966bff6c6c109d560151b7ad6bd
Huy Pham <huyducphamofficial@gmail.com>
2025-02-03 01:55:46 +0700

    12 | Set-up required params for 2FA

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -24,6 +25,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles;
     use Filterable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
