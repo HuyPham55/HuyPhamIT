05be0e637151be04850ff5fb8960fc237f845026
Huy Pham <huyducphamofficial@gmail.com>
2025-03-14 09:45:16 +0700

    22 | Implement destructure syntax in NewsItem

--- resources/ts/pages/home/components/components/NewsItem.vue
+++ resources/ts/pages/home/components/components/NewsItem.vue
@@ -13,11 +13,24 @@ const props = defineProps<Props>()
 
 const {post} = toRefs(props)
 
-const author = computed(() => post.value.author)
+/**
+ * Destructured refs
+ * Keep reactivity intact
+ */
+const {
+  title,
+  hash,
+  short_description,
+  publish_date,
+  view_count,
+  author,
+  tags,
+} = toRefs(post.value)
 
-const hasTags = computed<boolean>(() => !!post.value.tags?.length)
 
-const computedShowDescription = computed(() => !!post.value.short_description.length && props.detailedMode)
+const hasTags = computed<boolean>(() => !!(tags?.value?.length))
+
+const computedShowDescription = computed(() => !!short_description.value.length && props.detailedMode)
 
 </script>
 
@@ -25,13 +38,13 @@ const computedShowDescription = computed(() => !!post.value.short_description.le
   <div class="grid gap-4 pb-4 sm:grid-cols-10 md:grid-cols-10 md:gap-6 md:pb-6">
     <div class="sm:col-span-10 lg:col-span-10 flex flex-wrap gap-2">
       <h3>
-        <router-link :to="{name: 'post_detail', params: {hash : post.hash}}"
+        <router-link :to="{name: 'post_detail', params: {hash}}"
                      class="content-center font-semibold text-gray-900 hover:underline dark:text-white text-xl">
-          {{ post.title }}
+          {{ title }}
         </router-link>
       </h3>
       <div class="d-flex flex-wrap" v-if="hasTags">
-        <a v-for="tag in post.tags"
+        <a v-for="tag in tags"
            class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
            href="#">
           {{ tag.name }}
@@ -41,7 +54,7 @@ const computedShowDescription = computed(() => !!post.value.short_description.le
 
     <div v-if="computedShowDescription" class="sm:col-span-10 lg:col-span-10">
       <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-        {{ post.short_description }}
+        {{ short_description }}
       </p>
     </div>
     <div class="flex items-center space-x-2 sm:col-span-8 lg:col-span-9">
@@ -54,7 +67,7 @@ const computedShowDescription = computed(() => !!post.value.short_description.le
       </span>
       </div>
       <span class="text-gray-500 dark:text-gray-400">
-        {{ post.publish_date }}
+        {{ publish_date }}
       </span>
       <div class="flex flex-grow space-x-2 justify-end">
         <span class="font-medium text-gray-500 dark:text-gray-400 inline-flex items-center">
@@ -67,7 +80,7 @@ const computedShowDescription = computed(() => !!post.value.short_description.le
         </span>
         <span class="font-medium text-gray-500 dark:text-gray-400 inline-flex items-center">
           <i class="fa fa-eye mr-1 h-3"></i>
-          {{post.view_count}}
+          {{view_count}}
         </span>
       </div>
     </div>
