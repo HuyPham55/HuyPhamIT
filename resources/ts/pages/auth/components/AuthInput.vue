a94a27a96772659451738b59a30d398d34b9ebc8
Huy Pham <huyducphamofficial@gmail.com>
2025-04-21 17:09:13 +0700

    25 | Implement Vee-validate and perform form submit action

--- resources/ts/pages/auth/components/AuthInput.vue
+++ resources/ts/pages/auth/components/AuthInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import {toRefs} from "vue";
+import {toRefs, computed} from "vue";
+import {ErrorMessage, Field} from "vee-validate";
 
 const props = defineProps({
   name: {
@@ -29,6 +30,10 @@ const {
   placeholder,
   label,
 } = toRefs(props)
+
+const computedClass = computed(() => {
+  return `bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white`
+})
 </script>
 
 <template>
@@ -36,13 +41,16 @@ const {
     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" :for="name">
       {{ label }}
     </label>
-    <input
-      :id="name"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-      :name="name"
-      :placeholder="placeholder"
-      :autocomplete="autocomplete"
-      :type="type">
+    <Field :name="name" v-slot="{ field }">
+      <input
+        v-bind="field"
+        :id="name"
+        :placeholder="placeholder"
+        :type="type"
+        :class="computedClass"
+        :autocomplete="autocomplete">
+    </Field>
+    <ErrorMessage :name="name" as="small"/>
   </div>
 </template>
 
