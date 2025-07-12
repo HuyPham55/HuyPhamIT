c3b8e442e26ff484c71cc4720d82bfed3c049c0a
Huy Pham <huyducphamofficial@gmail.com>
2025-07-12 16:10:07 +0700

    25 | Update HTML structure in index.blade.php for improved app initialization

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,13 @@
 <html lang="{{app()->getLocale()}}" data-theme="dark">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     @vite(['resources/ts/app.ts'])
     <title>{{config('app.name')}}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset(cachedOption('site_favicon')) }}"/>
 </head>
-<body id="app">
+<body>
+    <div id="app"></div>
 </body>
 </html>
