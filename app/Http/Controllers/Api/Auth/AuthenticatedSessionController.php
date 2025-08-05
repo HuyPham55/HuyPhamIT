29ee5bb4a5672aed35d5e7c944b855b848e8e8dc
Huy Pham <huyducphamofficial@gmail.com>
2025-08-05 18:38:38 +0700

    25 | Refactor authentication logic to use Member guard for login and logout

--- app/Http/Controllers/Api/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Api/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Member;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -27,7 +28,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): Response
     {
-        Auth::guard('web')->logout();
+        Auth::guard(Member::GUARD_NAME)->logout();
 
         $request->session()->invalidate();
 
