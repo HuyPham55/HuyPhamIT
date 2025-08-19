a2883b40b68c2abf8366dfe54ded76d5fc34d0f9
Huy Pham <huyducphamofficial@gmail.com>
2025-08-19 17:49:26 +0700

    27 | Handle 404 errors and improve navigation in PostDetail and PostPreview components

--- resources/ts/pages/post-detail/PostDetail.vue
+++ resources/ts/pages/post-detail/PostDetail.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import {ref} from "vue";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import axios from "axios";
 import {PostDetailType} from "@/pages/post-detail/PostDetail";
 import PostDetailWrapper from "@/pages/post-detail/components/PostDetailWrapper.vue";
@@ -11,14 +11,22 @@ import PostDetailWrapper from "@/pages/post-detail/components/PostDetailWrapper.
 // })
 
 const route = useRoute();
-
+const router = useRouter();
 const post = ref<PostDetailType>();
 
 const loading = ref(false);
 const fetch = async function () {
   loading.value = true;
-  const {data: responseData} = await axios.get("/posts/" + route.params.hash)
-  post.value = responseData.data
+  try {
+    const {data: responseData} = await axios.get("/posts/" + route.params.hash)
+    post.value = responseData.data
+  } catch (e) {
+    if ((e as { status: number }).status === 404) {
+      await router.push({name: "NotFound"})
+      return
+    }
+    console.error(e)
+  }
   loading.value = false;
 }
 
--- resources/ts/pages/post-preview/PostPreview.vue
+++ resources/ts/pages/post-preview/PostPreview.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
 
 import {ref} from "vue";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import {PostDetailType} from "@/pages/post-detail/PostDetail";
 import axios from "axios";
 import PostDetailWrapper from "@/pages/post-detail/components/PostDetailWrapper.vue";
 
 const route = useRoute();
-
+const router = useRouter();
 const post = ref<PostDetailType>();
-
 const loading = ref(false);
 const fetch = async function () {
   loading.value = true;
-  // get URL
-  let fullPath = route.fullPath;
-  const {data: responseData} = await axios.get(fullPath)
-  post.value = responseData.data
+  try {
+    // get URL
+    let fullPath = route.fullPath;
+    const {data: responseData} = await axios.get(fullPath)
+    post.value = responseData.data
+  } catch (e) {
+    if ((e as { status: number }).status === 404) {
+      await router.push({name: "NotFound"})
+      return
+    }
+    console.error(e)
+  }
   loading.value = false;
 }
-
 fetch()
 
 </script>
